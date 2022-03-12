79fadd86d2a10c9ca9d3fa6aca72981074cc3173
avocadoclouds <rehana447@gmail.com>
2022-03-13 02:38:21 +0300

    update files

--- models/city.py
+++ models/city.py
@@ -20,4 +20,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"),  nullable=False)
-    #place = relationship("place", backref="cities", cascade="all, delete")
+    place = relationship("place", backref="cities", cascade="all, delete")
