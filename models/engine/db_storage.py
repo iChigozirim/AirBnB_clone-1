7348c2418ff58d3f7ca9abfdc792ce7999908973
iChigozirim <iamokwukwe@gmail.com>
2022-03-12 16:06:27 +0100

    Create a class that manages storage for hbnb models

---
+++ models/engine/db_storage.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Defines a class DBStorage."""
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+
+class DBStorage:
+    """Manages storage for hbnb models in MySQL database."""
+
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialize the class."""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
+
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """   """
+
+        if cls == None:
+            objs = self.__session.query(Amenity).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(State).all())
+            objs.exten(self.__session.query(User).all())
+        else:
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls).all()
+        dictionary = {}
+        for obj in objs:
+            key = "{}.{}".format(obj.__name__, obj.id)
+            dictionary[key] = obj
+        return dictionary
+
+
+    def new(self, obj):
+        """Adds the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commits all changes of the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes obj from the current database session if not None."""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create all tables in the database."""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=True)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    # def close(self):
+    #     """ Remove or close the session """
+    #     self.__session.close()
