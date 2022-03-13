5bab92741914966e387f311ca79f803332f8c77c
avocadoclouds <rehana447@gmail.com>
2022-03-13 05:40:00 +0300

    fix err

--- tests/test_console.py
+++ tests/test_console.py
@@ -22,7 +22,6 @@ class TestConsoleDocs(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-
     def test_console_module_docstring(self):
         ''' Test for the console.py module docstr. '''
         self.assertIsNot(console.__doc__, None,
@@ -30,9 +29,8 @@ class TestConsoleDocs(unittest.TestCase):
         self.assertTrue(len(console.__doc__) >= 1,
                         "console.py needs a docstring")
 
-
     def test_HBNBCommand_class_docstring(self):
-        ''' Test for the HBNBCommand class docstr '''
+        ''' Test for the HBNBCommand class docstr. '''
         self.assertIsNot(HBNBCommand.__doc__, None,
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
