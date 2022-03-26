4837f2150086a7cf80e7690433ce1fa96cfcb667
root <iamokwukwe@gmail.com>
2022-03-26 09:43:23 -0700

    Correct error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
 
-sudo apt update
-sudo apt install -y nginx
+sudo apt-get update
+sudo apt-get install -y nginx
 
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
