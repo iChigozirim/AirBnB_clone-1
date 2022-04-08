c84604b3ca294206fec4b0884f8b3cd3702038a5
root <iamokwukwe@gmail.com>
2022-04-08 15:40:03 -0700

    Remove HTML tags

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def home():
     """Displys 'Hello HBNB!'"""
-    return "<h1>Hello HBNB!</h1>"
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
