b7c5d485ebf64b19d3a0116877604f07eeb7fe4f
root <iamokwukwe@gmail.com>
2022-04-08 16:24:06 -0700

    Uncomment code block

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,7 +23,7 @@ def display_1():
     """Displays 'HBNB'"""
     return "HBNB"
 
-"""
+
 @app.route("/c/<text>", strict_slashes=False)
 def display_2(text):
     """Displays 'C' followed by the value of the text variable.
@@ -31,7 +31,7 @@ def display_2(text):
     """
     text = text.replace("_", " ")
     return “C {}”.format(text)
-"""
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
