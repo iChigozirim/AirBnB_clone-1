80362abf4ceb8c3e4ecfec18f3e72513f9307196
Chigozirim <iamokwukwe@gmail.com>
2022-06-24 01:04:45 +0100

    Quick fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 
 class FileStorage:
@@ -27,19 +35,17 @@ class FileStorage:
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        dictionary = {}
+        for key, obj in self.__objects.items():
+            dictionary[key] = obj.to_dict()
+        with open(self.__file_path, "w") as json_file:
+            json.dump(dictionary, json_file, ensure_ascii=False)
 
     def delete(self, obj=None):
         """Removes on instance from self.__objects if obj != None."""
         if obj is None:
             return
-        self.reload()
         for k, v in self.__objects.items():
             if obj == v:
                 del self.__objects[k]
@@ -47,14 +53,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
--- models/state.py
+++ models/state.py
@@ -1,34 +1,12 @@
 #!/usr/bin/python3
-""" State Module for HBNB project. """
-import models
-from os import getenv
-from models.city import City
+"""Define a class State that inherits from BaseModel."""
 from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import String
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='delete')
+class State(BaseModel):
+    """Represents a state.
+    Attributes:
+        name (str): The name of the state.
+    """
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """
-                getter attribute cities that returns the list of City
-                instances with state_id equals to the current
-                State.id => It will be the FileStorage
-                relationship between State and City
-            """
-            city_list = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    name = ""
