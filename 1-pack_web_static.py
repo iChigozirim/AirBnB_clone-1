32e421eb5e196ba1d7ee6d17d2cc5fd0ca62efe8
Igweamaka Chigozirim <iamokwukwe@gmail.com>
2022-06-26 23:05:57 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,9 @@
 """
 Fabric script that generates a .tgz archive from the contents of a folder.
 """
-from fabric.api import *
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
 
 
 def do_pack():
@@ -10,6 +12,14 @@ def do_pack():
     Generates a .tgz archive from the contents of the web_static folder on an
     AirBnB Clone repo.
     """
-    name = ""
-    run("tar -cv archive.tgz /web_static/*.")
+    dt = datetime
+    file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
+    )
 
+    if isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+        if local("tar -czvf {} web_static".format(file_name)).failed is True:
+            return None
+    return file_name
